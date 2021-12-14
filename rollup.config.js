c64e0861723338970ae068e75d4d6c25a843274c
Mervin <mengqing723@gmail.com>
2021-12-14 11:06:57 +0800

    :construction_worker: build

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+const pkg = require('./package.json')
+import { terser } from 'rollup-plugin-terser'
+
+const banner =
+`/*!
+ * mp-axios v${pkg.version}
+ * Promise based HTTP client for miniprogram.
+ * (c) 2021 Mervin [https://github.com/Meqn/mp-request-promise]
+ * Released under the MIT License.
+*/`
+
+
+export default {
+  input: 'lib/index.js',
+  plugins: [
+    terser()
+  ],
+  output: {
+    file: `index.js`,
+    format: 'es',
+    banner
+  }
+}
